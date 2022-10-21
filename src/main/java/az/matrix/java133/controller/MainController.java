a19349a0c298c353e92f4b6dd4a99dca442d0a0d
seferovramin7 <seferovramin7@gmail.com>
2022-10-21 18:41:11 +0400

    Initial commit

---
+++ src/main/java/az/matrix/java133/controller/MainController.java
@@ -0,0 +1,22 @@
+package az.matrix.java133.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/udemy")
+    public void getMyText() {
+        System.out.println(" udemy udemy !, I'm udemy udemy");
+    }
+
+
+    @GetMapping("/matrix")
+    public String getMyTexttt() {
+        return " Hello Matrix !, I'm matrix matrix";
+    }
+
+}
+//github.com/python-projects/spring-boot/wiki/Spring-Boot-2.0-Migration-Guide
---
+++ src/test/java/az/matrix/java133/SpringLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package az.matrix.java133;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
